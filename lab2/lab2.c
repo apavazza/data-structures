b5c9bae8067b7b44dfa18daed6d0451ca17bf667
amadeop <amadeo.pavazza@gmail.com>
2022-10-23 01:19:35 +0200

    lab2 second commit

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -7,36 +7,156 @@
 
 #define MAX_STR_LEN 128
 
-bool addToStart(Person x, Node* n);
+struct _node;
+typedef struct _node Node;
+Node* createElement(void);
+bool addToStart(Node* el, Node* n);
+bool addToEnd(Node* el, Node* n);
+char* inputLastName(void);
+void printHelp(void);
 void printList(Node* n);
-bool addToEnd(Person x, Node* n);
-Node* find(Person x, Node* n);
-Node* findPrev(Person x, Node* n);
-bool delete(Person x, Node* n);
-
-typedef struct _person
-{
-	char firstName[MAX_STR_LEN];
-	char lastName[MAX_STR_LEN];
-	char yearOfBirth[4];
-} Person;
+Node* find(char* x, Node* n);
+Node* findPrev(char* x, Node* n);
+bool delete(Node* n);
+void deleteAll(Node* n);
 
 typedef struct _node
 {
-	Person el;
+	char firstName[MAX_STR_LEN];
+	char lastName[MAX_STR_LEN];
+	int yearOfBirth;
 	Node* next;
 } Node;
 
-bool addToStart(Person x, Node* n)
-{
-	Node* new = (Node* )malloc(sizeof(Node));
-	if (new == NULL) return false;
+int main(void) {
+	char command[11] = { 0 };
+	Node* head = (Node*)malloc(sizeof(Node));
+	head->next = NULL;
 
-	new->el = x;
-	new->next = n->next;
-	n->next = new;
+	printf("Linked List Addresser\n\n");
+	printHelp();
+
+	while (true)
+	{
+		printf(" > ");
+		scanf(" %s", command);
+		if (strcmp(command, "addtostart") == 0 || strcmp(command, "as") == 0)
+		{
+			if (!addToStart(createElement(), head))
+			{
+				printf("Person could not be added\n");
+			}
+		}
+		else if (strcmp(command, "addtoend") == 0 || strcmp(command, "ae") == 0)
+		{
+			if (!addToEnd(createElement(), head))
+			{
+				printf("Person could not be added\n");
+			}
+		}
+		else if (strcmp(command, "print") == 0 || strcmp(command, "p") == 0)
+		{
+			printList(head);
+		}
+		else if (strcmp(command, "find") == 0 || strcmp(command, "f") == 0)
+		{
+			if (!find(inputLastName(), head))
+			{
+				printf("The person could not be found\n");
+			}
+		}
+		else if (strcmp(command, "delete") == 0 || strcmp(command, "d") == 0)
+		{
+			if (!delete(inputLastName(), head))
+			{
+				printf("The person could not be found\n");
+			}
+		}
+		else if (strcmp(command, "help") == 0 || strcmp(command, "h") == 0)
+		{
+			printHelp();
+		}
+		else if (strcmp(command, "quit") == 0 || strcmp(command, "q") == 0)
+		{
+			printf("Quitting application...");
+			// oslobadjanje prostora
+			deleteAll(head);
+			break;
+		}
+		else
+		{
+			printf("Command not recognized! Please try again\n");
+		}
+	}
+	return EXIT_SUCCESS;
+}
+
+Node* createElement(void)
+{
+	char firstName[MAX_STR_LEN] = { 0 };
+	char lastName[MAX_STR_LEN] = { 0 };
+	int yearOfBirth = 0;
+	Node* new = (Node*)malloc(sizeof(Node));
+	if (new == NULL) return NULL;
+
+	printf("First name: ");
+	scanf(" %s", firstName);
+	printf("Last name: ");
+	scanf(" %s", lastName);
+	printf("Year of birth: ");
+	scanf(" %d", &yearOfBirth);
+
+	strcpy(new->firstName, firstName);
+	strcpy(new->lastName, lastName);
+	new->yearOfBirth = yearOfBirth;
+	new->next = NULL;
+
+	return new;
+}
+
+bool addToStart(Node* el, Node* n)
+{
+	if (!el) return false;
+
+	el->next = n->next;
+	n->next = el;
 
 	return true;
+}
+
+bool addToEnd(Node* el, Node* n)
+{
+	if (!el) return false;
+
+	while (n->next != NULL)
+	{
+		n = n->next;
+	}
+
+	el->next = NULL;
+	n->next = el;
+
+	return true;
+}
+
+char* inputLastName()
+{
+	char lastName[MAX_STR_LEN] = { 0 };
+	printf("Last name: ");
+	scanf(" %s", lastName);
+	return &lastName;
+}
+
+void printHelp(void)
+{
+	printf("Usage:\n\n"
+		"\taddtostart, as \tAdds a person to the start of the list\n"
+		"\taddtoend, ae \tAdds a person to the end of the list\n"
+		"\tprint, p \tPrints all elevents of the list\n"
+		"\tfind, f \tSearches for a person in the list\n"
+		"\tdelete, d \tDelets a person from the list\n"
+		"\thelp, h \tPrints this help message\n"
+		"\tquit, q \tQuits the application\n\n");
 }
 
 void printList(Node* n)
@@ -44,50 +164,36 @@ void printList(Node* n)
 	n = n->next; // preskacemo head
 	while (n != NULL)
 	{
-		printf("%s %s\n", n->el.firstName, n->el.lastName);
+		printf(" %s %s %d\n",n->firstName, n->lastName, n->yearOfBirth);
 		n = n->next;
 	}
 }
 
-bool addToEnd(Person x, Node* n)
-{
-	Node* new = (Node*)malloc(sizeof(Node));
-	if (new == NULL) return false;
-
-	while (n->next != NULL)
-	{
-		n = n->next;
-	}
-	
-	new->el = x;
-	new->next = NULL;
-	n->next = new;
-
-	return true;
-}
-
-Node* find(Person x, Node* n)
+Node* find(char* x, Node* n)
 {
 	n = n->next; // preskacemo head
-	while (n != NULL && n->el.lastName != x.lastName)
+	while (n != NULL && strcmp(n->lastName, x) != 0)
 	{
 		n = n->next;
 	}
+	if (n == NULL) return NULL;
+
+	printf("FOUND: %s %s %d", n->firstName, n->lastName, n->yearOfBirth);
 
 	return n;
 }
 
-Node* findPrev(Person x, Node* n)
+Node* findPrev(char* x, Node* n)
 {
-	while (n->next != NULL && n->next->el.lastName != x.lastName)
+	while (n->next != NULL && (strcmp(n->next->lastName, x) != 0))
 	{
 		n = n->next;
 	}
-
+	if (n->next == NULL) return NULL;
 	return n;
 }
 
-bool delete(Person x, Node* n)
+bool delete(char* x, Node* n)
 {
 	Node* prev = NULL;
 	prev = findPrev(x, n);
@@ -99,7 +205,13 @@ bool delete(Person x, Node* n)
 	return true;
 }
 
-int main(void) {
-	Node* head = (Node*)malloc(sizeof(Node));
-	head->next = NULL;
+void deleteAll(Node* n)
+{
+	Node* temp = NULL;
+
+	while ((temp = n) != NULL)
+	{
+		n = n->next;
+		free(temp);
+	}
 }
